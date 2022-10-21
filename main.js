23d7689804345d54a365ef38a906bd16d0b23e11
SamirCortesSoho <edwin@soho.com.uy>
2022-10-21 15:32:51 -0500

    renderizar productos del array

--- main.js
+++ main.js
@@ -4,42 +4,43 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail')
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
 carritoIcon.addEventListener('click', toggleAside);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
-    if (!isAsideClosed){
+    if (!isAsideClosed) {
         aside.classList.add('inactive')
     }
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu(){
+function toggleMobileMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
-    if (!isAsideClosed){
-        aside.classList.add('inactive') 
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
     }
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleAside () {
+function toggleAside() {
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
     if (!isDesktopMenuClosed) {
         desktopMenu.classList.add('inactive')
     }
 
     const ismobileMenuclosed = mobileMenu.classList.contains('inactive');
-    if (!ismobileMenuclosed){
+    if (!ismobileMenuclosed) {
         mobileMenu.classList.add('inactive')
     }
     aside.classList.toggle('inactive');
 }
 
-const  productList =[];
+const productList = [];
 productList.push({
     name: 'bike',
     price: 120,
@@ -76,10 +77,48 @@ productList.push({
     image: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Basketball.jpeg',
 });
 
-for (product of productList) {
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
+function renderProducts (arr) {
+    for (product of arr) {
 
-    const img = document.createElement('img');
-    img.
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+    
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+    
+    
+    }
 }
+
+renderProducts(productList);
